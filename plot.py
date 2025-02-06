504f3b539bf720ba093c8b927205daa43f877d15
Gustavo <gustavoquintero9105@gmail.com>
2025-02-06 10:17:22 -0500

    Updated on 02-06-2025 at 10:17

--- plot.py
+++ plot.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 size_img = 0.6
-plt.rcParams.update({'font.size': 8})
+plt.rcParams.update({'font.size': 12})
 plt.rcParams['figure.figsize'] = [size_img * 6.4,size_img * 4.8]
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.tick_params(axis='both',direction='in')
 
-data1 = np.loadtxt("alpha09/output_1_10.txt",delimiter=" ")
-data2 = np.loadtxt("alpha09/output_10_1000.txt",delimiter=" ")
-data3 = np.loadtxt("alpha09/output_1000_100000.txt",delimiter=" ")
+data1 = np.loadtxt("alpha08/output_1_10.txt",delimiter=" ")
+data2 = np.loadtxt("alpha08/output_10_1000.txt",delimiter=" ")
+data3 = np.loadtxt("alpha08/output_1000_100000.txt",delimiter=" ")
 
 x1 = data1[:,0]
 y1 = data1[:,2]
@@ -23,24 +23,18 @@ y3 = data3[:,2]
 
 
 plt.plot(x1,y1,":o",ms=3,lw=1)
-plt.xlabel("$n$",fontsize=12)
-plt.ylabel("$s_n$",fontsize=12)
-# plt.ylim(-0.2,4)
-plt.yticks(np.arange(0.0,4.1,1))
-plt.savefig("alpha09/mnqfig1a.pdf",bbox_inches = "tight")
+plt.xlabel("$n$",fontsize=16)
+plt.ylabel("$s_n$",fontsize=16)
+plt.savefig("alpha08/mnqfig1a.pdf",bbox_inches = "tight")
 plt.close()
 
 plt.plot(x2,y2,":o",ms=3,lw=1)
-plt.xlabel("$n$",fontsize=12)
-plt.ylabel("$s_n$",fontsize=12)
-# plt.ylim(-0.2,4)
-plt.yticks(np.arange(0.0,0.09,0.02))
-plt.savefig("alpha09/mnqfig1b.pdf",bbox_inches = "tight")
+plt.xlabel("$n$",fontsize=16)
+plt.ylabel("$s_n$",fontsize=16)
+plt.savefig("alpha08/mnqfig1b.pdf",bbox_inches = "tight")
 plt.close()
 
 plt.plot(x3,y3,":o",ms=3,lw=1)
-plt.xlabel("$n$",fontsize=12)
-plt.ylabel("$s_n$",fontsize=12)
-# plt.ylim(-0.2,4)
-plt.yticks(np.arange(0.0,0.0021,0.0005))
-plt.savefig("alpha09/mnqfig1c.pdf",bbox_inches = "tight")
+plt.xlabel("$n$",fontsize=16)
+plt.ylabel("$s_n$",fontsize=16)
+plt.savefig("alpha08/mnqfig1c.pdf",bbox_inches = "tight")
