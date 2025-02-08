c9fded59329b6a5c9bd94549cec7f3b07508de31
Gustavo <gustavoquintero9105@gmail.com>
2025-02-08 09:28:23 -0500

    Updated on 02-08-2025 at 09:28

--- plot.py
+++ plot.py
@@ -8,9 +8,9 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.tick_params(axis='both',direction='in')
 
-data1 = np.loadtxt("alpha08/output_1_10.txt",delimiter=" ")
-data2 = np.loadtxt("alpha08/output_10_1000.txt",delimiter=" ")
-data3 = np.loadtxt("alpha08/output_1000_100000.txt",delimiter=" ")
+data1 = np.loadtxt("alpha05/output_1_10.txt",delimiter=" ")
+data2 = np.loadtxt("alpha05/output_10_1000.txt",delimiter=" ")
+data3 = np.loadtxt("alpha05/output_1000_100000.txt",delimiter=" ")
 
 x1 = data1[:,0]
 y1 = data1[:,2]
@@ -21,20 +21,19 @@ y2 = data2[:,2]
 x3 = data3[:,0]
 y3 = data3[:,2]
 
-
 plt.plot(x1,y1,":o",ms=3,lw=1)
 plt.xlabel("$n$",fontsize=16)
 plt.ylabel("$s_n$",fontsize=16)
-plt.savefig("alpha08/mnqfig1a.pdf",bbox_inches = "tight")
+plt.savefig("alpha05/mnqfig5a.pdf",bbox_inches = "tight")
 plt.close()
 
 plt.plot(x2,y2,":o",ms=3,lw=1)
 plt.xlabel("$n$",fontsize=16)
 plt.ylabel("$s_n$",fontsize=16)
-plt.savefig("alpha08/mnqfig1b.pdf",bbox_inches = "tight")
+plt.savefig("alpha05/mnqfig5b.pdf",bbox_inches = "tight")
 plt.close()
 
 plt.plot(x3,y3,":o",ms=3,lw=1)
 plt.xlabel("$n$",fontsize=16)
 plt.ylabel("$s_n$",fontsize=16)
-plt.savefig("alpha08/mnqfig1c.pdf",bbox_inches = "tight")
+plt.savefig("alpha05/mnqfig5c.pdf",bbox_inches = "tight")
